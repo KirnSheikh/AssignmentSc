0dff4d672a20d0a25f350696e7c4595f0f9dde3f
kir <kir@172.16.108.118>
2016-12-17 22:18:48 +0500

    first commit

---
+++ src/MediatorPatternDemo.java
@@ -0,0 +1,13 @@
+
+public class MediatorPatternDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  User robert = new User("Robert");
+	      User john = new User("John");
+
+	      robert.sendMessage("Hi! John!");
+	      john.sendMessage("Hello! Robert!");
+	}
+
+}
